1fd6e5d272bccb866f4a470d797cd4c932da9699
RUVAIS P <146152602+ruvais-p@users.noreply.github.com>
2025-12-19 22:04:23 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String supabaseUrl = 'https://gbvtbemrhwgdkqgpjpvo.supabase.co';
+  static const String supabaseUrl = 'YOUR_URL';
   static const String supabaseAnonKey =
-      "sb_publishable_Yu3Adu--dbbUzz4Tr1hgEQ_wlXMbPDM";
+      "YOUR_KEY";
 }
